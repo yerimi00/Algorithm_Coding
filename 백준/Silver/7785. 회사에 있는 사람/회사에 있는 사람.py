489a51d27e3d7635e55fc7212689bacdede0af92
Lee Yerim <129266668+yerimi00@users.noreply.github.com>
2026-03-24 21:14:09 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 172 ms, Memory: 42656 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+office = set()
+
+for _ in range(n):
+    name, status = input().split()
+    if status == "enter":
+        office.add(name)
+    else:
+        office.discard(name)
+
+for name in sorted(office, reverse=True):
+    print(name)
